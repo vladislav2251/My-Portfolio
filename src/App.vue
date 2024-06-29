f29b872537047dc44247ca4ec8a808b0b78b72ec
vladislav2251 <vladdidyk1985@gmail.com>
2024-06-29 13:10:21 +0300

    last version official

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,32 @@
                 <Footer />
         </main>
 </template>
-<script setup lang="ts">
+<script setup>
         import Header from '@/components/layouts/header.vue';
         import Footer from '@/components/layouts/footer.vue';
+
+        import Lenis from 'lenis';
+
+        const lenis = new Lenis({
+        duration: 1.2,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), 
+        direction: 'vertical',
+        gestureDirection: 'vertical',
+        smooth: true,
+        mouseMultiplier: 1,
+        smoothTouch: false,
+        touchMultiplier: 2,
+        infinite: false,
+        })
+        
+        lenis.on('scroll', ({ scroll, limit, velocity, direction, progress }) => {
+        console.log({ scroll, limit, velocity, direction, progress })
+        })
+
+        function raf(time) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+        }
+
+        requestAnimationFrame(raf)
 </script>
